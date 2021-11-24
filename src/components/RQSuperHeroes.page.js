77e6d28fdc068548fe5f0db0b40d1112dbdd1a9d
Anandhan <anandhanedom@gmail.com>
2021-11-24 17:01:20 +0530

    featL custom hooks

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,10 +1,4 @@
-import { useQuery } from "react-query";
-import axios from "axios";
-
-const fetchSuperheroes = async () => {
-  const res = await axios.get("http://localhost:4000/superheroes");
-  return res.data;
-};
+import { useSuperHeroesData } from "../hooks/useSuperHeroesData";
 
 export const RQSuperHeroesPage = () => {
   const onSuccess = (data) => {
@@ -15,25 +9,8 @@ export const RQSuperHeroesPage = () => {
     console.log("Perform side effects after encountering an error", err);
   };
 
-  const { isLoading, data, isError, error, refetch, isFetching } = useQuery(
-    "super-heroes",
-    fetchSuperheroes,
-    // {
-    //   cacheTime: 300000, // default
-    //   staleTime: 0, // default
-    //   refetchOnMount: true, // default
-    //   refetchOnWindowFocus: true, // default
-    //   refetchInterval: false, // default - provide ms here
-    //   refetchIntervalInBackground: false, // default
-    // }
-    // {
-    //   enabled: false,
-    // }
-    {
-      onSuccess,
-      onError,
-    }
-  );
+  const { isLoading, data, isError, error, refetch, isFetching } =
+    useSuperHeroesData(onSuccess, onError);
 
   if (isLoading || isFetching) {
     return <h2>Loading...</h2>;
@@ -47,7 +24,8 @@ export const RQSuperHeroesPage = () => {
     <>
       <h2>React Query Super Heroes Page</h2>
       <button onClick={refetch}>Fetch heroes</button>
-      {data && data.map((hero) => <div key={hero.name}>{hero.name}</div>)}
+      {/* {data && data.map((hero) => <div key={hero.name}>{hero.name}</div>)} */}
+      {data && data.map((hero) => <div key={hero}>{hero}</div>)}
     </>
   );
 };
---
+++ src/hooks/useSuperHeroesData.js
@@ -0,0 +1,33 @@
+import { useQuery } from "react-query";
+import axios from "axios";
+
+const fetchSuperheroes = async () => {
+  const res = await axios.get("http://localhost:4000/superheroes");
+  return res.data;
+};
+
+export const useSuperHeroesData = (onSuccess, onError) => {
+  return useQuery(
+    "super-heroes",
+    fetchSuperheroes,
+    // {
+    //   cacheTime: 300000, // default
+    //   staleTime: 0, // default
+    //   refetchOnMount: true, // default
+    //   refetchOnWindowFocus: true, // default
+    //   refetchInterval: false, // default - provide ms here
+    //   refetchIntervalInBackground: false, // default
+    // }
+    // {
+    //   enabled: false,
+    // }
+    {
+      onSuccess,
+      onError,
+      select: (data) => {
+        const superHeroNames = data.map((hero) => hero.name);
+        return superHeroNames;
+      },
+    }
+  );
+};
